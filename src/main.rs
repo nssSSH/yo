1921af460ee82d784ce4561111b3979ead66a1b9
nssSSH <218080797+nssSSH@users.noreply.github.com>
2025-12-14 17:22:35 +0530

    uhh did something

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use clap::{Args, Parser};
-use colored::{Colorize, ColoredString};
+use clap::Parser;
+use colored::{Colorize};
 /// Simple program to display a colored message on startup of terminal
 #[derive(Parser, Debug)]
 #[command(version, about)]
@@ -14,6 +14,17 @@ struct Init {
 
 fn main(){
     let args = Init::parse();
-    println!("{:?}", args);
+    let output = match args.color.to_lowercase().as_str() {
+        "red" => args.message.red(),
+        "blue" => args.message.blue(),
+        "yellow" => args.message.yellow(),
+        "green" => args.message.green(),
+        "cyan" => args.message.cyan(),
+        "purple" => args.message.purple(),
+        "magenta" => args.message.magenta(),
+        &_ => args.message.white()
+    };
+
+    println!("{output}");
 
 }
